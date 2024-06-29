81435a5e339e904ec6f1da55cf3c2011746dc995
Lyba <faisallyba06@gmail.com>
2024-06-29 18:08:36 +0500

    If-else

--- jscode2.js
+++ jscode2.js
@@ -6,3 +6,40 @@ count = 5;
 
 var box;
 box = "Lyba";
+
+//if-else
+
+
+let age = 18;
+if (age >= 18)
+    {
+        console.log("You are an adult");
+    }
+
+
+//2nd example
+
+
+let score = 30;
+
+
+if (score >= 90)
+    {
+        console.log("You are passed with A+ Grade");
+    }
+else if (score >= 80)
+    {
+        console.log("You are passed with A Grade");
+    }
+else if (score >= 70)
+    {
+        console.log("You are passed with B Grade");
+    }
+else if (score >= 60)
+    {
+        console.log("You are passed with C Grade");
+    }    
+else
+{
+    console.log("Sorry, You are Failed");
+}
