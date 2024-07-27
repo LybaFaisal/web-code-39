c68603dc38aba89e59d18e28d8b82d530ec8835b
Lyba <faisallyba06@gmail.com>
2024-07-27 18:03:47 +0500

    Length-Concat

--- Arrays.Js/arrays.js
+++ Arrays.Js/arrays.js
@@ -10,20 +10,42 @@ let arr = ['Sparrow','Eagle','HummingBird','Crow','Peacock'];
 
 //To add 
 arr.splice(1, 0, 'Tiger')
-console.log(arr);
+console.log(arr.toString());  //using .tostring();
 
 //to remove
 
 arr.splice(3 , 1 );
-console.log(arr);
+console.log(arr.toString());
 
 //to replace
 
 arr.splice(3, 1 , 'Snake');
-console.log(arr);
+console.log(arr.join('*'));
 
 // Acessing through loop
 
 for(let j = 0 ; j < 5 ; j++){
     console.log(arr[j]);
 }
+
+//through arr.length
+
+console.log(`The following array through length property is:`);
+
+for(let j = 0 ; j < arr.length ; j++){
+    console.log(arr[j]);
+}
+
+console.log(`Concatenation of the following array=`);
+//concatenation in arrays=>
+    
+let arr1 = [1,3,5,7,9];
+let arr2 = [2,4,6,8,10];
+let arr_combined = arr1.concat(arr2);
+console.log(arr_combined);
+
+//converting to string
+
+console.log(`The combined array in the string is=`);
+
+console.log(arr_combined.toString());
