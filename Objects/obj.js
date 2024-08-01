892785952cc9cc4f7266456416e2670cf085ac0d
Lyba <faisallyba06@gmail.com>
2024-08-01 14:38:17 +0500

    Adding properties-OBJECTS

--- Objects/obj.js
+++ Objects/obj.js
@@ -5,3 +5,14 @@ console.log(person.course);
 console.log(person.name);
 console.log(person); //showing full obj
 
+//Adding more properties to an object
+
+let cake = {
+    cakeTitle: 'Red Velvet' ,
+    colour: 'Red',
+    Taste: 'yummm',
+};
+
+cake.layers = 3;
+
+console.log(cake);
