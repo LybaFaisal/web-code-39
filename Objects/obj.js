6583feafd73103bca089c128673989f0fa8f1bb6
Lyba <faisallyba06@gmail.com>
2024-08-01 14:27:11 +0500

    Objects

---
+++ Objects/obj.js
@@ -0,0 +1,7 @@
+//Creating a simple object
+
+let person = {name:'Lyba' , course: 'ComputerScience' , city:'Faisalabad'};
+console.log(person.course);
+console.log(person.name);
+console.log(person); //showing full obj
+
